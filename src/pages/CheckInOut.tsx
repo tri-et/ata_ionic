fb906c19bfe20df55b5a8c26bdb558a5d1fe7993
tri-et <tripham178@gmail.com>
2020-11-10 16:34:12 +0700

    check in out

--- src/pages/CheckInOut.tsx
+++ src/pages/CheckInOut.tsx
@@ -2,17 +2,59 @@ import React from 'react';
 import {
   IonContent,
   IonPage,
+  IonCard,
+  IonCardContent,
+  IonButton,
+  IonLabel,
+  IonIcon,
+  IonDatetime,
 } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { Grid } from '@material-ui/core';
 import ATAHeader from '../components/header/ATAHeader';
+import ATAClock from '../components/clock/ATAClock';
+import { logInOutline, logOutOutline } from 'ionicons/icons';
 import './CheckInOut.css';
 
 const CheckInOut: React.FC = () => {
+  var date = new Date();
   return (
     <IonPage>
       <ATAHeader></ATAHeader>
       <IonContent fullscreen>
-        <ExploreContainer name='Tab 1 page' />
+        <IonCard>
+          <IonCardContent className='card-content'>
+            <Grid container justify='center'>
+              <Grid
+                container
+                direction='column'
+                alignItems='center'
+                className='margin-bottom'
+              >
+                <ATAClock />
+                <IonLabel color='primary' className='date'>
+                  <IonDatetime
+                    displayFormat='D - MMM - YYYY'
+                    value={date.toISOString()}
+                  ></IonDatetime>
+                </IonLabel>
+              </Grid>
+              <IonButton expand='full' className='button-check' shape='round'>
+                <IonIcon className='icon-check' icon={logInOutline}></IonIcon>
+                Check In
+              </IonButton>
+              <IonButton
+                expand='full'
+                className='button-check'
+                shape='round'
+                disabled={true}
+              >
+                <IonIcon className='icon-check' icon={logOutOutline}></IonIcon>
+                Check Out
+              </IonButton>
+            </Grid>{' '}
+            <IonLabel color='primary'>Please proceed to Check In</IonLabel>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
